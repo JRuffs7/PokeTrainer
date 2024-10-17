a176b0c0c2eb947316d97bb61d4e80831c78f091
Justin Ruffell <22224556+JRuffs7@users.noreply.github.com>
2024-10-16 22:49:52 -0400

    Command Locks and Daycare (#29)

--- services/commandlockservice.py
+++ services/commandlockservice.py
@@ -11,6 +11,9 @@ def AddLock(serverId: int, userId: int):
 def DeleteLock(serverId: int, userId: int):
   return commandlockda.DeleteLock(serverId, userId)
 
+def DeleteAllLocks():
+  return commandlockda.DeleteAllLocks()
+
 
 def IsEliteFourLocked(serverId: int, userId: int):
   return commandlockda.CheckEliteFourLock(serverId, userId)
